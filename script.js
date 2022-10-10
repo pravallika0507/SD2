9ec9d429f9697a153f471ea935f3287dde146852
pravallika0507 <107581314+pravallika0507@users.noreply.github.com>
2022-10-10 11:56:23 +0100

    conditions and loops

---
+++ script.js
@@ -0,0 +1,52 @@
+var name = "pravallika";
+var age = 25;
+
+if (age = 25) {
+    //block of code
+}
+
+if (age = 25) {
+    //block of code
+}
+else {
+    //block of code
+}
+
+if (age = 25) {
+    //block of code
+}
+else if (name = "pravallika") {
+    //block of code
+}
+else {
+    //block of code
+}
+
+switch (age) {
+    case 20:
+        //block of code
+        break;
+    case 21:
+        //block of code
+        break;
+    default:
+        //block of code
+}
+
+for (var i = 0; i < 10; i++) {
+    //block of code
+}
+
+var names = ["Lisa", "Arturo", "Kimia", "Kevin"];
+for (var name of names) {
+    document.write(name + "<br>");
+}
+
+while (name = "pravallika") {
+    //block of code
+}
+
+function myFunction() {
+    //code to execute
+    return value;
+}
