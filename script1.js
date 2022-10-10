ed062068a9457f0201c1fb4234212eac33f85ae6
pravallika0507 <107581314+pravallika0507@users.noreply.github.com>
2022-10-10 11:46:30 +0100

    starting javascript

---
+++ script1.js
@@ -0,0 +1,9 @@
+document.write("Hello, World!" + "<br>");
+var name = "pravallika";
+var age = 25;
+var student = true;
+document.write('Name: ' + name + '<br>');
+document.write('Age: ' + age + '<br>');
+document.write('Is Student: ' + student);
+console.log(name);
+
---
+++ script2.js
@@ -0,0 +1,6 @@
+function msg() {
+    document.write("You clicked the button!");
+}
+function change() {
+    document.write("Hello, World!")
+}
